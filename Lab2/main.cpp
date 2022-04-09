73f742c47d77eae98777afe4f4588274693faf1f
Polzovat123 <42420479+Polzovat123@users.noreply.github.com>
2022-04-09 16:01:34 +0500

    Clear

--- Lab2/main.cpp
+++ Lab2/main.cpp
@@ -17,7 +17,7 @@ layout (location = 0) in vec3 Position;                                         
                                                                                     \n\
 uniform mat4 gWorld;                                                                \n\
                                                                                     \n\
-void main(){                                                                         \n\                                                                                   \n\
+void main(){                                                                        \n\
     gl_Position = gWorld * vec4(Position, 1.0);                                     \n\
 }";
 
